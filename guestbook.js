39a120f73a6a4e37b5f84c3da29cc86e38a7450d
Nethsara Siyum <nethsarasiyum2@gmail.com>
2023-02-12 23:28:35 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/2cb621d8-ca1e-4273-8dad-13cc262c6f94/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/read-guestbook-entries-sequence.json`,
       dataType: 'json'
     });
   },
